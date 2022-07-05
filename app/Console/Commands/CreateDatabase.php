af57ba8814b7743ab7d7d32a4832b1c6c8533c18
Platinum <platinumemirate@gmail.com>
2022-07-05 15:54:27 +0100

    rm installation route after install

--- app/Console/Commands/CreateDatabase.php
+++ app/Console/Commands/CreateDatabase.php
@@ -1,51 +1,52 @@
 <?php
 
 namespace App\Console\Commands;
+
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Inspiring;
 
 class CreateDatabase extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'make:database';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'make:database';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create a new database';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Create a new database';
 
-    public function __construct()
-    {
-        parent::__construct();
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+    $new_db_name = config('database.connections.mysql.database');
+    $new_mysql_username = config('database.connections.mysql.username');
+    $new_mysql_password = config('database.connections.mysql.password');
+    $new_mysql_host = config('database.connections.mysql.host');
+
+    $conn = mysqli_connect($new_mysql_host, $new_mysql_username, $new_mysql_password);
+    if (!$conn) {
+      return false;
     }
-
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-            $new_db_name = config('database.connections.mysql.database');
-            $new_mysql_username = config('database.connections.mysql.username');
-            $new_mysql_password = config('database.connections.mysql.password');
-            $new_mysql_host = config('database.connections.mysql.host');
-
-            $conn = mysqli_connect($new_mysql_host,$new_mysql_username,$new_mysql_password);
-            if(!$conn ) {
-                return false;
-            }
-            $sql = 'CREATE Database IF NOT EXISTS '.$new_db_name;
-            $exec_query = mysqli_query( $conn, $sql);
-            if(!$exec_query) {
-                die('Could not create database: ' . mysqli_error($conn));
-            }
+    $sql = 'CREATE Database IF NOT EXISTS ' . $new_db_name;
+    $exec_query = mysqli_query($conn, $sql);
+    if (!$exec_query) {
+      die('Could not create database: ' . mysqli_error($conn));
     }
+  }
 }
--- app/Http/Controllers/InstallationController.php
+++ app/Http/Controllers/InstallationController.php
@@ -9,41 +9,43 @@ use Illuminate\Support\Facades\Schema;
 
 class InstallationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        // if(!file_exists(public_path('installation.php'))){
-        //     return redirect()->route('welcome');
-        // }
-        return view('installation');
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    if (!file_exists(public_path('installation.php'))) {
+      return redirect()->route('welcome');
     }
+    return view('installation');
+  }
 
-    public function store(Request $request)
-    {
-        switch($request->action){
-            case 'create_database': Artisan::call('make:database');
-                break;
-            case 'migrations': Artisan::call('migrate');
-                break;
-            case 'finish': 
-                $database_name = config('database.connections.mysql.database');
-                $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME =  ?";
-                $db = DB::select($query, [$database_name]);
-                if(empty($db)) {
-                    return redirect()->back();
-                }elseif('write something') {
-                    return redirect(route('settings'));
-                }
-                        // if(){
-                        //     unlink(public_path('installation.php'));
-                        //     return redirect()->route('welcome');
-                        // }else 
-                break;
+  public function store(Request $request)
+  {
+    switch ($request->action) {
+      case 'create_database':
+        Artisan::call('make:database');
+        break;
+      case 'migrations':
+        Artisan::call('migrate');
+        break;
+      case 'finish':
+        $database_name = config('database.connections.mysql.database');
+        $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME =  ?";
+        $db = DB::select($query, [$database_name]);
+        if (empty($db)) {
+          return redirect()->back();
+        } elseif ('write something') {
+          return redirect(route('settings'));
         }
-        return redirect()->route('installation');  
+        // if(){
+        //     unlink(public_path('installation.php'));
+        //     return redirect()->route('welcome');
+        // }else 
+        break;
     }
+    return redirect()->route('installation');
+  }
 }

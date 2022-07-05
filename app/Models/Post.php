46df38a3ced476c0d5b9044aba009f8243e2d1bf
Platinum <platinumemirate@gmail.com>
2022-07-05 12:39:13 +0100

    relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,10 +12,10 @@ class Post extends Model
 
    public function user()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo(User::class);
     }
     public function category()
     {
-        return $this->belongsTo('App\Category');
+        return $this->belongsTo(Category::class);
     }
 }

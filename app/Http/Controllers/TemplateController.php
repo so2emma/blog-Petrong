b2163acf38eed0adf7e532fcb699034f949806d4
Platinum <platinumemirate@gmail.com>
2022-07-09 00:45:20 +0100

    SettingsSeeder

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Settings;
+use App\Models\Template;
+use Illuminate\Http\Request;
+
+class TemplateController extends Controller
+{
+  public function index()
+  {
+    $in = new InstallationController;
+    if ( !$in->hd_c() & $in->hm_() & !file_exists(public_path('installation.php')))
+      return redirect(route('welcome'));
+    $templates = Template::all();
+    return view('setup.choosetemplate', compact('templates'));
+  }
+
+  public function choose(Request $request){
+    Settings::where('key', 'template')
+      ->update(['value', $request->template_id]);
+  }
+}

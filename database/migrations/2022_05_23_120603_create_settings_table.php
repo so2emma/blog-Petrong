7ed84ff93d0b1593a55a553817177ef2032ca70c
Platinum <platinumemirate@gmail.com>
2022-07-08 13:34:22 +0100

    settings

--- database/migrations/2022_05_23_120603_create_settings_table.php
+++ database/migrations/2022_05_23_120603_create_settings_table.php
@@ -16,27 +16,27 @@ class CreateSettingsTable extends Migration
   {
     Schema::create('settings', function (Blueprint $table) {
       $table->bigIncrements('id');
-      $table->string('name');
+      $table->string('key');
       $table->text('value')->nullable();
       $table->timestamps();
     });
     DB::table('settings')->insert(array(
-      array('id' => 1, 'name' => "company_name", 'value' => null),
-      array('id' => 2, 'name' => "company_address", 'value' => null),
-      array('id' => 3, 'name' => "company_email", 'value' => null),
-      array('id' => 4, 'name' => "company_logo", 'value' => null),
-      array('id' => 5, 'name' => "company_facebook", 'value' => null),
-      array('id' => 6, 'name' => "company_twitter", 'value' => null),
-      array('id' => 7, 'name' => "company_linkedin", 'value' => null),
-      array('id' => 8, 'name' => "company_instagram", 'value' => null),
-      array('id' => 9, 'name' => "company_about", 'value' => null),
-      array('id' => 10, 'name' => "show_company_name", 'value' => null),
-      array('id' => 11, 'name' => "show_logo", 'value' => null),
-      array('id' => 12, 'name' => "company_about_image", 'value' => null),
-      array('id' => 13, 'name' => "company_vission_mission", 'value' => null),
-      array('id' => 14, 'name' => "company_vission_mission_image", 'value' => null),
-      array('id' => 15, 'name' => "enable_blog", 'value' => null),
-      array('id' => 16, 'name' => "company_phone", 'value' => null),
+      array('id' => 1, 'key' => "blog_name", 'value' => null),
+      array('id' => 2, 'key' => "blog_address", 'value' => null),
+      array('id' => 3, 'key' => "blog_email", 'value' => null),
+      array('id' => 4, 'key' => "blog_logo", 'value' => null),
+      array('id' => 5, 'key' => "blog_facebook", 'value' => null),
+      array('id' => 6, 'key' => "blog_twitter", 'value' => null),
+      array('id' => 7, 'key' => "blog_linkedin", 'value' => null),
+      array('id' => 8, 'key' => "blog_instagram", 'value' => null),
+      array('id' => 9, 'key' => "blog_about", 'value' => null),
+      array('id' => 10, 'key' => "show_blog_name", 'value' => null),
+      array('id' => 11, 'key' => "show_logo", 'value' => null),
+      array('id' => 12, 'key' => "blog_about_image", 'value' => null),
+      array('id' => 13, 'key' => "blog_vission_mission", 'value' => null),
+      array('id' => 14, 'key' => "blog_vission_mission_image", 'value' => null),
+      array('id' => 15, 'key' => "enable_blog", 'value' => null),
+      array('id' => 16, 'key' => "blog_phone", 'value' => null),
     ));
   }
 

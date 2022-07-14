5887256e8a82b714b4014075d23df9fc2409a76f
Platinum <platinumemirate@gmail.com>
2022-07-14 15:31:04 +0100

    template_is

--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -9,4 +9,10 @@ class Settings extends Model
   protected $fillable = [
     'key', 'value'
   ];
+
+  static function template_is(){
+    $template_id = Settings::where('key', 'template')->first()->value;
+    $template = Template::find($template_id);
+    return $template->folder;
+  }
 }

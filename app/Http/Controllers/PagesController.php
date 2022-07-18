fde71b7214e40105ab53034d578977fc9ffe99a2
Platinum <platinumemirate@gmail.com>
2022-07-18 14:06:17 +0100

    sunness blog index

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,36 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
 
 class PagesController extends Controller
 {
-   
-    public function blog()
-    {   
-        $posts = Post::paginate(5);
-        $categories = Category::all();
-        return view('blog.blog', compact('posts', 'categories'));
-    }
 
-    public function show($id)
-    {
-      $categories = Category::all();
-        $post = Post::findOrfail($id);
-        return view('blog.singlepost',compact('post','categories'));
-    }
-    
-    public function postcategory($id)
-    {
-      $categories = Category::all();
-        $posts = Post::where('category_id', $id)->paginate(5);
-        return view('blog.category', compact('posts', 'categories'));
-    }
+  public function blog()
+  {
+    $posts = Post::paginate(5);
+    return view('blog.blog', compact('posts'));
+  }
 
-    public function featuredposts($id)
-    {
-        $posts = Post::where('id', $id)->paginate(5);
-        return view('service', compact('posts'));
-    }
+  public function show($id)
+  {
+    $categories = Category::all();
+    $post = Post::findOrfail($id);
+    return view('blog.singlepost', compact('post', 'categories'));
+  }
+
+  public function postcategory($id)
+  {
+    $categories = Category::all();
+    $posts = Post::where('category_id', $id)->paginate(5);
+    return view('blog.category', compact('posts', 'categories'));
+  }
+
+  public function featuredposts($id)
+  {
+    $posts = Post::where('id', $id)->paginate(5);
+    return view('service', compact('posts'));
+  }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = [
-        'name',
-       ];
+  protected $fillable = [
+    'name',
+  ];
 
-       public function posts()
-    {
-        return $this->hasMany(Post::class);
-    }
+  public function posts()
+  {
+    return $this->hasMany(Post::class);
+  }
 }

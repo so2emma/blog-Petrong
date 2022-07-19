b9810f8744829f68c8635fe42222e573546bdb3d
Platinum <platinumemirate@gmail.com>
2022-07-19 16:09:43 +0100

    header state

--- resources/views/layouts/platonic/header.blade.php
+++ resources/views/layouts/platonic/header.blade.php
@@ -1,61 +1,64 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+  <div class="container-fluid px-3 px-md-4">
+    <a class="navbar-brand" href="{{ url('/') }}">
+      <h1 class="h4">{{ config('app.name', 'Blog') }}</h1>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-      <a class="navbar-brand" href="{{ url('/') }}">
-        <h1 class="h4">{{ config('app.name', 'Blog') }}</h1>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navContent">
-        <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <!-- <li class="nav-item">
+    <div class="collapse navbar-collapse" id="navContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- <li class="nav-item">
             <a class="nav-link active"
               aria-current="page" href="{{ route('welcome') }}"
               >Home</a>
           </li> -->
-        </ul>
+      </ul>
 
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('about') }}">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('blog') }}">Blog</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Categories
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              @forelse($categories->whereNotIn('name',['slider','team']) as $category)
-                <li><a class="dropdown-item text-capitalize" href="{{ route('post.category', $category->id) }}">{{$category->name}}</a></li>
-              @empty
-              @endforelse
-            </ul>
-          </li>
-          @guest
-          @else
-          <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              {{ Auth::user()->name }}
-            </a>
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a>
-              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('about') }}">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('blog') }}">Blog</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categories
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            @forelse($categories as $category)
+            <li><a class="dropdown-item text-capitalize" href="{{ route('post.category', $category->id) }}">{{$category->name}}</a></li>
+            @empty
+            @endforelse
+          </ul>
+        </li>
+
+        @if(Route::has('login') && Auth::check())
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ Auth::user()->name }}
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a></li>
+            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
-              </a>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-              </form>
-            </div>
-          </li>
-          @endguest
-        </ul>
-      </div>
+              </a></li>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
+          </ul>
+        </li>
+        @elseif (Route::has('login') && !Auth::check())
+        <li class="nav-item">
+          <a href="{{route('login')}}" class="nav-link"> Login</a>
+        </li>
+        @endif
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
